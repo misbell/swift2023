ad9843cef61b1fc0258a5ab91c92ccb297cb3e31
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-09-29 16:47:19 -0400

    Initial Commit

---
+++ Swift2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift2023
+//
+//  Created by Michael Prenez-Isbell on 9/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift2023/Swift2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift2023App.swift
+//  Swift2023
+//
+//  Created by Michael Prenez-Isbell on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
